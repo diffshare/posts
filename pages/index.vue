31deec955a0af3f531937abd2d33e3f4cc067e98
diffshare <diffshare@gmail.com>
2021-02-07 13:59:35 +0900

    Topページを修正

--- pages/index.vue
+++ pages/index.vue
@@ -1,75 +1,38 @@
 <template>
   <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        posts
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+    <li v-for="post of posts" :key="post.slug">
+      <NuxtLink :to="{ name: 'posts-slug', params: { slug: post.slug } }">
+<!--        <img :src="article.img" />-->
+        <div>
+          <h2>{{ post.title }}</h2>
+          <p>{{ post.description }}</p>
+        </div>
+      </NuxtLink>
+    </li>
   </div>
 </template>
 
-<script lang="ts">
-import Vue from 'vue'
+<script>
+// import Vue from 'vue'
+// import {Context} from "@nuxt/types";
 
-export default Vue.extend({})
+export default {
+  async asyncData({ $content, params }) {
+    const posts = await $content('posts').fetch();
+
+    // const [prev, next] = await $content('articles')
+    //   .only(['title', 'slug'])
+    //   .sortBy('createdAt', 'asc')
+    //   .fetch()
+
+    return {
+      posts,
+      // prev,
+      // next
+    }
+  },
+}
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
