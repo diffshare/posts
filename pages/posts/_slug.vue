69860d6bc2a46e993860c295aca5a2d31be69a61
diffshare <diffshare@gmail.com>
2021-02-07 13:14:20 +0900

    first commit

---
+++ pages/posts/_slug.vue
@@ -0,0 +1,23 @@
+<template>
+  <article>
+    <nuxt-content :document="post"/>
+    <p>Post last updated: {{ post.updatedAt }}</p>
+  </article>
+</template>
+
+<script>
+export default {
+  name: "_slug.vue",
+
+  async asyncData({$content, params}) {
+    const post = await $content('posts', params.slug).fetch();
+
+    return {post}
+  }
+}
+</script>
+
+<style scoped>
+
+
+</style>
